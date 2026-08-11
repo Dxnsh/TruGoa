4dd4441ad69fd6649829c45d4009c9320aaacc3c
Dinesh Patil <dineshpatil0942@gmail.com>
2026-08-11 12:10:04 +0530

    Add Vercel Web Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./pages/HomePage/Homepage";
 import ExplorePage from "./pages/ExplorePage/ExplorePage";
@@ -41,6 +42,7 @@ function App() {
         <Route path="/contact"     element={<ContactPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
